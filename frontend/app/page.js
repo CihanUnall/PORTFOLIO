5a5b8be6b4aa3a18e9f144757fb0f37dbbe0562b
CihanUnall <cihanunal_20@hotmail.com>
2025-08-03 16:41:30 +0200

    homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,34 +1,8 @@
-"use client";
-import Image from "next/image";
-import "./styles/dashboard.scss";
+import React from "react";
+import Home from "./home/page.jsx";
 
-export default function Dashboard() {
-  return (
-    <div className="main-container">
-      <div className="text1">
-        <h2>Web Development</h2>
-      </div>
-      <div className="bild">
-        <Image
-          src="/cihan.png"
-          alt="cihan picture"
-          className="cihanbild"
-          width={900}
-          height={1000}
-        />
-      </div>
-      <div className="text2">
-        <h3>Hello </h3>
-        <h4>It's C.Unal a design wizerd</h4>
-        <br />
-        <p>
-          I built this portfolio interface entirely from scratch using Next.js,
-          SCSS, and HTML. No pre-built UI libraries or visual assets were used —
-          except for my own photo. The goal was to demonstrate my front-end
-          development skills and ability to build clean, functional interfaces
-          purely through code.
-        </p>
-      </div>
-    </div>
-  );
+function page() {
+  return <Home />;
 }
+
+export default page;
