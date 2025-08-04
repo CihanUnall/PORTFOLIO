ef61e31c954c9d23c365a8f1ab66d163e72da366
CihanUnall <cihanunal_20@hotmail.com>
2025-08-04 10:04:00 +0200

    resume

--- frontend/app/document/page.jsx
+++ frontend/app/document/page.jsx
@@ -3,7 +3,7 @@
 export default function Documents() {
   return (
     <main className="flex h-full items-center justify-center bg-gray-50 px-6 py-12">
-      <div className="w-[80%] h-[40%] bg-white rounded-lg shadow-lg p-12 flex flex-col items-center justify-center">
+      <div className="w-4/5 h-2/5 bg-white rounded-lg shadow-lg p-12 flex flex-col items-center justify-center">
         <h1 className="text-[1.5rem] font-semibold text-gray-900 mb-6 text-center">
           Access the Documents
         </h1>
@@ -12,7 +12,7 @@ export default function Documents() {
           below.
         </p>
         <a
-          href="#"
+          href="/document"
           className="w-30 h-10 flex items-center justify-center bg-blue-400 text-white font-semibold py-3 px-8 rounded-md hover:bg-blue-700 transition-colors duration-300"
         >
           Click here
