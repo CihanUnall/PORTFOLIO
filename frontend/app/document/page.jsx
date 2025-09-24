ef6b578284616a1cf9e2d52a37c21c980ea3e58c
CihanUnall <cihanunal_20@hotmail.com>
2025-09-24 12:22:29 +0200

    Link anderungen

--- frontend/app/document/page.jsx
+++ frontend/app/document/page.jsx
@@ -8,72 +8,72 @@ const certificates = [
   {
     title: "Zertifizierte Weiterbildung zum Webentwickler - Full Stack",
     image: "/dci.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Bachelor of Arts in International Business",
     image: "/bachelers.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "English",
     image: "/sprache.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Deutsch",
     image: "/sprache.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Html & CSS",
     image: "/certificate.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Dreamweaver",
     image: "/certificate.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Adobe Photoshop",
     image: "/certificate.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Adobe Flash",
     image: "/certificate.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Adobe Fireworks",
     image: "/certificate.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Microsoft System Expert",
     image: "/certificate.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Cultural Diversity",
     image: "/goodhabitz.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Scrum",
     image: "/goodhabitz.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Soziale Intelligenz",
     image: "/goodhabitz.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
   {
     title: "Teamwork",
     image: "/goodhabitz.png",
-    link: "https://share.eu.internxt.com/d/sh/folder/fa75b9e5-2ee8-4ff9-85c2-73ee7c097edb/5de42aebde80c37056912d56f8ee0adedceba6a80d6ebf7ec1f6ad2c9aa1c0a1",
+    link: "https://secure.internxt.com/sh/folder/gPGenzuRReKcYu5Ip822Kw/z4RBY1vX",
   },
 ];
 
